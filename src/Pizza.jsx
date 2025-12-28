7a08cce552486fc1102bd4ef41a3b0c607455a6a
Shish <shishfruitwala29@gmail.com>
2025-12-28 13:16:55 +0530

    api and hooks

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,8 +1,14 @@
 const Pizza = (props) => {
   return (
-    <div className="pizza">
+    <div
+      className="pizza"
+      onClick={() => {
+        console.log("Hi");
+      }}
+    >
       <h1>{props.name}</h1>
       <h1>{props.description}</h1>
+      <img src={props.image} alt={props.name} />
     </div>
   );
 };
